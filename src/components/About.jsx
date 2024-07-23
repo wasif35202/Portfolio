63629255152e880f69f0f3bb238f51091ac699ed
wasif35202 <145279916+wasif35202@users.noreply.github.com>
2024-07-23 12:46:50 +0500

    23july2024

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
       <h1 className="my-20 text-center text-4xl">
-        About
-        <span className="text-neutral-500"> Me</span>
+        ABOUT
+        <span className="text-neutral-500"> ME</span>
       </h1>
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2 lg:p-8">
